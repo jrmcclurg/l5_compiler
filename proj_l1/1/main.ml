47b3a5b744397e16ee8dcd616d7b9040ac2fd609
Jedidiah McClurg <jed@jrmcclurg.com>
2012-03-30 20:48:53 +0000

    Fixed to compile for TLAB target

--- proj_l1/1/main.ml
+++ proj_l1/1/main.ml
@@ -4,9 +4,8 @@ Arg.parse [] (fun x -> filename := x) "Parser Generator Generator v. 1.0\nby Jed
 let in_stream = if (!filename="") then stdin else (open_in !filename) in
 let lexbuf = Lexing.from_channel in_stream in
 let result = Parser.main Lexer.token lexbuf in
-(* Ast.print_grammar 0 result;
-print_newline(); *)
-Code.generate_code !filename result;
+Ast.print_grammar 0 result;
+print_newline();
 print_string "\nDone";
 print_newline();
 exit 0
