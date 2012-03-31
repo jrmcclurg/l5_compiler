fb9a5a8365835af9d4517c504e1363672d775aac
Jedidiah McClurg <jed@jrmcclurg.com>
2012-03-31 06:00:11 +0000

    Code generation framework is mostly in place. A simple working a.out is generated

--- proj_l1/1/main.ml
+++ proj_l1/1/main.ml
@@ -1,12 +1,21 @@
+let assembly_file_name = "prog.S";;
+let runtime_file_name  = "runtime.c";;
+
 let filename = ref "";;
-Arg.parse [] (fun x -> filename := x) "Parser Generator Generator v. 1.0\nby Jedidiah McClurg";;
+Arg.parse [] (fun x -> filename := x) "L1 Compiler v. 1.0\nby Jedidiah McClurg";;
 
 let in_stream = if (!filename="") then stdin else (open_in !filename) in
 let lexbuf = Lexing.from_channel in_stream in
 let result = Parser.main Lexer.token lexbuf in
 Ast.print_program result;
 print_newline();
-Code.compile_program result;
+let out1 = (open_out runtime_file_name) in
+Code.generate_runtime out1;
+close_out out1;
+let out2 = (open_out assembly_file_name) in
+Code.compile_program out2 result;
+close_out out2;
+Code.compile_assembly ();
 print_string "\nDone";
 print_newline();
 exit 0
